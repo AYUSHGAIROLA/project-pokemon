a54ab2e3252f4ae1cff7cd99086ba1a0193f2b86
AYUSHGAIROLA <gairolaayush4444@gmail.com>
2023-12-07 10:01:44 +0530

    feat(pokemon): pokemon dshboard

--- pokemon-project/src/app/app.module.ts
+++ pokemon-project/src/app/app.module.ts
@@ -5,11 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonBodyComponent } from '../components/pokemon-body/pokemon-body.component';
 import { PokemonHeaderComponent } from '../components/pokemon-header/pokemon-header.component';
+import {
+  MatCardModule
+} from '@angular/material/card';
+// import {
+//   MatSpinner
+// } from '@angular/material/progress-spinner';
+import { AbilitiesFilterPipe } from 'src/pipes/abilitiesFilter.pipe';
+import { SearchPipe } from 'src/pipes/search.pipe';
+import { TypeFilterPipe } from 'src/pipes/typeFilter.pipe';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent, PokemonBodyComponent, PokemonHeaderComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, PokemonBodyComponent, PokemonHeaderComponent,
+    SearchPipe,
+    TypeFilterPipe,
+    AbilitiesFilterPipe,
+  ],
+  imports: [BrowserModule, AppRoutingModule, MatCardModule, CommonModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [PokemonBodyComponent, PokemonHeaderComponent]
 })
 export class AppModule {}
--- pokemon-project/src/services/pokemon.service.ts
+++ pokemon-project/src/services/pokemon.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { PokeAPI, PokemonDetails } './pokemon-utils';
+import { PokeAPI, PokemonDetails } from './pokemon-utils';
 @Injectable({
   providedIn: 'root',
 })
