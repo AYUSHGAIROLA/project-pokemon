a61eb494acee913d8e56258ef01ddd47bdff7261
AYUSHGAIROLA <gairolaayush4444@gmail.com>
2023-12-07 10:28:52 +0530

    feat(pokemon): add api calls and cards

--- pokemon-project/src/app/app.module.ts
+++ pokemon-project/src/app/app.module.ts
@@ -8,14 +8,20 @@ import { PokemonHeaderComponent } from '../components/pokemon-header/pokemon-hea
 import {
   MatCardModule
 } from '@angular/material/card';
-// import {
-//   MatSpinner
-// } from '@angular/material/progress-spinner';
+import {
+  MatToolbarModule
+} from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AbilitiesFilterPipe } from 'src/pipes/abilitiesFilter.pipe';
 import { SearchPipe } from 'src/pipes/search.pipe';
 import { TypeFilterPipe } from 'src/pipes/typeFilter.pipe';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, PokemonBodyComponent, PokemonHeaderComponent,
@@ -23,7 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     TypeFilterPipe,
     AbilitiesFilterPipe,
   ],
-  imports: [BrowserModule, AppRoutingModule, MatCardModule, CommonModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, MatCardModule, CommonModule, HttpClientModule, MatToolbarModule, FormsModule, ReactiveFormsModule, MatInputModule, MatIconModule, MatFormFieldModule, MatSelectModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
   exports: [PokemonBodyComponent, PokemonHeaderComponent]
--- pokemon-project/src/components/pokemon-header/pokemon-header.component.ts
+++ pokemon-project/src/components/pokemon-header/pokemon-header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Results } from 'src/services/pokemon-utils';
 
 @Component({
   selector: 'app-pokemon-header',
@@ -6,10 +7,96 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-header.component.scss']
 })
 export class PokemonHeaderComponent implements OnInit {
+  @Output() searchChange = new EventEmitter();
+  @Output() typeSelected = new EventEmitter();
+  @Output() abilitiesSelected = new EventEmitter();
 
-  constructor() { }
+  abilities: Array<string>;
+  types: Array<string>;
+  pokemonList: Array<Results>;
+  search: string;
+  currentType: string;
+  currentAbilities: Array<string>;
 
-  ngOnInit(): void {
+  @Input() set pokemons(pokemons: Results[]) {
+    if (pokemons !== this.pokemonList) {
+      this.pokemonList = pokemons;
+
+      // Get types and abilities from each pokemon
+      this.pokemonList.forEach(pokemon => {
+        this.setPokemonAbilities(pokemon);
+        this.setPokemonTypes(pokemon);
+      });
+    }
   }
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.abilities = [];
+    this.types = [];
+  }
+
+  /**
+   * Called when a search field is updated
+   */
+  searchEvent(search): void {
+    // check for cleared search
+    if (search === '') {
+      this.search = search;
+    }
+    this.searchChange.emit(this.search);
+  }
+
+  /**
+   * Called when a type has been selected
+   */
+  onTypeSelected(): void {
+    if (this.currentType) {
+      this.typeSelected.emit(this.currentType);
+    } else {
+      this.typeSelected.emit('');
+    }
+  }
+
+  /**
+   * Called when ability filter changes
+   */
+  onAbilitySelected(): void {
+    if (this.currentAbilities && this.currentAbilities.length) {
+      this.abilitiesSelected.emit(this.currentAbilities);
+    } else {
+      this.abilitiesSelected.emit('');
+    }
+  }
+
+  /**
+   * Grabs pokemons abilities and adds to array
+   */
+  setPokemonAbilities(pokemon: Results): void {
+    if (pokemon) {
+      pokemon.details.abilities.forEach(ability => {
+        const abilityName = ability.ability.name;
+        if (!this.abilities.includes(abilityName)) {
+          this.abilities.push(abilityName);
+          this.abilities.sort();
+        }
+      });
+    }
+  }
+
+  /**
+   * Grabs a pokemons types and adds to array
+   */
+  setPokemonTypes(pokemon: Results): void {
+    if (pokemon) {
+      pokemon.details.types.forEach(type => {
+        const typeName = type.type.name;
+        if (!this.types.includes(typeName)) {
+          this.types.push(typeName);
+          this.types.sort();
+        }
+      });
+    }
+  }
 }
